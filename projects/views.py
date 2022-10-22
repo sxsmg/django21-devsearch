630076437ddffe3c8dc03dcbe3bc150c8dfd3ab2
sxsm <thesgadyal@gmail.com>
2022-10-23 01:08:48 +0530

    added templates

--- projects/views.py
+++ projects/views.py
@@ -5,8 +5,7 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return  HttpResponse("here are our projects")
-
+    return  render(request, "projects.html")
 
 def project(request, pk):
     return  HttpResponse("Single Project" + " " + str(pk))
