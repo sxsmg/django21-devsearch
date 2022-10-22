544755071d98f66154d46d9630e5a9497b6bb697
sxsm <thesgadyal@gmail.com>
2022-10-22 23:54:44 +0530

    added dynamic page url

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -8,7 +8,11 @@ def projects(request):
     return  HttpResponse("here are our projects")
 
 
+def project(request, pk):
+    return  HttpResponse("Single Project" + " " + str(pk))
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', projects, name="projects"),
+    path('project/<str:pk>', project, name="project"),
 ]
