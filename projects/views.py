8727e1525f9a9d3e5c487e94f238fee9c1ed073d
sxsm <thesgadyal@gmail.com>
2022-10-23 15:59:08 +0530

    added dynamic loading of project

--- projects/views.py
+++ projects/views.py
@@ -3,12 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id':'1',
+        'title':'Ecommerce Website',
+        'description':'Fully Functional Ecommerce website'
+    },
+    {
+        'id':'2',
+        'title':'Portfolio Website',
+        'description':'This website represents my portfolio'
+    },
+    {
+        'id':'3',
+        'title':'Social Network',
+        'description':'End to end fully featured social network'
+    },
+]
 
 def projects(request):
     page = "Projects"
     number = 10
-    context = {'page': page, 'number': number}
+    context = {'page': page, 'number': number, 'projects': projectsList}
     return  render(request, "projects/projects.html", context)
 
 def project(request, pk):
-    return  render(request, "projects/single-project.html")
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return  render(request, "projects/single-project.html", {'project':projectObj})
