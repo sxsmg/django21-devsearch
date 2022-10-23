2e4ae53f7d98004eddf0fc4d0d7bb73bb159cd4f
sxsm <thesgadyal@gmail.com>
2022-10-24 01:49:13 +0530

    projects descriptions dynamic

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Project
 
 projectsList = [
     {
@@ -22,14 +23,11 @@ projectsList = [
 ]
 
 def projects(request):
-    page = "Projects"
-    number = 10
-    context = {'page': page, 'number': number, 'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return  render(request, "projects/projects.html", context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-    return  render(request, "projects/single-project.html", {'project':projectObj})
+    projectObj = Project.objects.get(id=pk)
+    
+    return  render(request, "projects/single-project.html", {'project':projectObj })
