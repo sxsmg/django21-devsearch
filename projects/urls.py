6e5d521db1a1f8ec6124512db5bee50a416cfcca
sxsm <thesgadyal@gmail.com>
2022-11-06 21:51:20 +0530

    completed CRUD

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import project, projects, createProject
+from .views import project, projects, createProject, updateProject, deleteProject
 
 urlpatterns = [
     path('', projects, name="projects"),
     path('project/<str:pk>/', project, name="project"),
     path('create-project/', createProject, name="create-project"),
+
+    path('update-project/<str:pk>/', updateProject, name="update-project" ),
+    
+    path('delete-project/<str:pk>/', deleteProject, name="delete-project" ),
+    
 ]
--- projects/views.py
+++ projects/views.py
@@ -27,3 +27,24 @@ def createProject(request):
 
     context = {'form':form}
     return render(request, "projects/project_form.html", context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form':form}
+    return render(request, "projects/project_form.html", context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == "POST":
+        project.delete()
+        return redirect('projects')
+    context = {"object":project}
+    return render(request, 'projects/delete_template.html', context)
