970676daf0939877e7d07bacbc33dae3dd9c7c13
sxsm <thesgadyal@gmail.com>
2022-11-09 01:59:13 +0530

    added profiles

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import profiles, userProfile
 
 urlpatterns = [
-    path('', profiles, name="profiles"),
-    path('profile/<str:pk>/', userProfile, name="user-profile"), 
-
-    ]
     
+    path('', profiles, name="profiles"),
+    path('profile/<str:pk>/', userProfile, name="user-profile"),
+    ]
+   
