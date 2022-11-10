b9b81a80ddf923dccd6a26c007c48141405fe14e
sxsm <thesgadyal@gmail.com>
2022-11-10 21:00:14 +0530

    added few signals

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 import uuid
 # Create your models here.
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True,blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
@@ -24,7 +25,7 @@ class Profile(models.Model):
                             primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -38,3 +39,5 @@ class Skill(models.Model):
     
     def __str__(self):
         return str(self.name)
+
+
